ae5845312e0beb7d0416b79a5cf4fa4b5083bd57
Neil A. Kumar <denkrishna@gmail.com>
2021-10-21 22:14:17 -0600

    added cls matlab file

---
+++ cls.m
@@ -0,0 +1,8 @@
+%% Generic Reset Command By: Neil A. Kumar
+close all;      % Close all open windows / plots
+clear;          % Clear the workspace of any variables
+clc;            % Clear the command line
+
+% Disabled
+format long g;  % Reset command window formatting
+set(0,'DefaultFigureWindowStyle','docked') % Docks created Figures
