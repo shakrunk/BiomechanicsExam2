e580417e4dbeb9cfef11f8e8e820b07cc130c626
shakrunk <denkrishna@gmail.com>
2021-10-25 13:20:39 -0600

    fixing branch discrepencies

--- mech_main.m
+++ mech_main.m
@@ -35,7 +35,7 @@ function [out] = mech_main (varargin)
     for i = 1: 1: bar.NElem % loop through elements
         UncLoad = UncLoad + bar.EndLoad(i); % Calculates uncontrained end load
         out.UncLoad(i) = UncLoad;
-        out.UncMDef(i) = int_def(UncLoad,bar.Leng(i),bar.Area1(i),bar.Area2(i),bar.Modu1(i),bar.Modu2(i),bar.Nistp);
+        [ out.UncMDef(i), out.UncTDef(i) ] = int_def(UncLoad,bar.Leng(i),bar.Area1(i),bar.Area2(i),bar.Modu1(i),bar.Modu2(i),bar.Alph(i),bar.DeltT(i),bar.Nistp);
     
         % Inconstant Area: integral of [P / A(x) E(x)] dx
         %    linspace(Al, Ar, nsteps)
