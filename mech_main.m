71032ce56fab0e81e4c9072d0b3d8bfd32fc41e1
linea02 <91618635+linea02@users.noreply.github.com>
2021-10-26 20:28:39 -0600

    Update mech_main.m

--- mech_main.m
+++ mech_main.m
@@ -37,14 +37,7 @@ function [out] = mech_main (varargin)
     for i = 1: 1: bar.NElem % loop through elements
         UncLoad = UncLoad + bar.EndLoad(i); % Calculates uncontrained end load
         out.UncLoad(i) = UncLoad;
-        [ out.UncMDef(i), out.UncTDef(i) ] = int_def(UncLoad,bar.Leng(i),bar.Area1(i),bar.Area2(i),bar.Modu1(i),bar.Modu2(i),bar.Alph(i),bar.DeltT(i)-bar.initT,bar.Nistp);
-    
-        % Inconstant Area: integral of [P / A(x) E(x)] dx
-        %    linspace(Al, Ar, nsteps)
-        %    linspace(0, L2, nsteps)
-        %    trapz() 
-        %    cumtrap()
-        
+        [ out.UncMDef(i), out.UncTDef(i) ] = int_def(UncLoad,bar.Leng(i),bar.Area1(i),bar.Area2(i),bar.Modu1(i),bar.Modu2(i),bar.Alph(i),bar.DeltT(i)-bar.initT,bar.Nistp);    
     end
     disp([func, 'Done!']); %lgf
     
