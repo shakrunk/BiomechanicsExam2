0367a929c4bca90100cb136559808046fbf5baab
Neil A. Kumar <62001481+shakrunk@users.noreply.github.com>
2021-11-16 13:37:01 -0700

    Update cls.m
    format reset in cls command

--- cls.m
+++ cls.m
@@ -7,8 +7,8 @@
 % Enabled Commands
 close all;      % Close all open windows / plots
 clear;          % Clear the workspace of any variables
+format short e;  % Reset command window formatting
 clc;            % Clear the command line
 
 % Disabled Commands
-% format long g;  % Reset command window formatting
 % set(0,'DefaultFigureWindowStyle','docked') % Docks created Figures
