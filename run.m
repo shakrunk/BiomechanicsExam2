06b52c360e1040e996c1166b47739f9a43b183c8
linea02 <linea.gutierrez@ucdenver.edu>
2021-11-18 16:08:55 -0700

    small

--- run.m
+++ run.m
@@ -96,8 +96,4 @@ end
 
 fprintf("\n<strong>## End of Progam</strong>\n");
 
-%% Program Workspace / Debug Station
-    % A place to mess around with ideas if unsure where it will be placed
-    % organizationally. Ideally there should be nothing in this section
-    % once the file is complete.
     
